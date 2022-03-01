f806a1dba8f6ea5c4439de5528fcb97f67dcf286
Alan Wu <alanjialongwu@gmail.com>
2022-03-01 16:35:29 +1100

    Added hover functionality and reset functionality

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Etch-And-Sketch
+
+A basic implementation of an Etch and Sketch pad. 
+Hover over the canvas to colour the squares!
+Click on the **clear canvas** button to create a blank canvas.
+Click on the **change canvas** button to clear the canvas and change the number of squares in each row and column of the canvas.
--- script.js
+++ script.js
@@ -4,5 +4,40 @@ container.style['grid-template-columns'] = 'repeat(16, 1fr)';
 for(let i = 0; i < 16*16; i++){
     const div = document.createElement('div');
     div.setAttribute('class', 'square');
+    div.addEventListener('mouseenter', function(e){
+        this.style['background-color'] = 'black';
+    });
     container.appendChild(div);
 }
+
+function restart(){
+    let num = parseInt(prompt('Enter the number of squares per side for the new grid!'));
+    while( !(num <= 100 && num >= 0)){
+        num = parseInt(prompt('Please enter a number between 0 and 100.'));
+    }
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+    container.style['grid-template-rows'] = `repeat(${num}, 1fr)`;
+    container.style['grid-template-columns'] = `repeat(${num}, 1fr)`;
+    for(let i = 0; i < num*num; i++){
+        const div = document.createElement('div');
+        div.setAttribute('class', 'square');
+        div.addEventListener('mouseenter', function(e){
+            this.style['background-color'] = 'black';
+        });
+        container.appendChild(div);
+    }
+}
+function clear(){
+    const children = container.childNodes;
+    children.forEach((child)=>{
+        child.style['background-color'] = 'white';
+    })
+}
+
+const btn1 = document.querySelector('.restart');
+btn1.addEventListener('click', restart)
+
+const btn2 = document.querySelector('.clear');
+btn2.addEventListener('click', clear);
